9de72af059ff59d6f1b23da166cee68b640383a2
AkiraPJ <nicolas.morimoto@gmail.com>
2022-03-22 19:02:20 -0300

    forca

---
+++ forcafuncs.h
@@ -0,0 +1,11 @@
+#define TAMANHO_PALAVRA 20
+
+void abertura();
+void chuta();
+void desenhaforca();
+void escolhepalavra();
+void adicionapalavras();
+int jachutou(char letra);
+int chuteserrados();
+int enforcou();
+int acertou();
